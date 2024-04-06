b9109f85b4746e53dad375c2d57d3fa6cccb435a
Rian Delou <riandelou63@hotmail.com>
2024-04-05 22:18:24 -0300

    finish

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
 const user = document.getElementById("username");
 const enqueueButton = document.getElementById("push-button");
-const shiftButton = document.getElementById("shift-button");
-const printQueue = document.getElementById("queue");
+const dequeueButton = document.getElementById("shift-button");
+const printQueue = document.getElementById("queue-screen");
 
 let queue = [];
+let aux = 0;
 
 const enqueue = (value) => {
     queue.push(value);
-}
+};
 
 const dequeue = () => {
-    queue.shift();
-}
+    aux = 0;
+
+    if (queue.length > 0) {
+        queue.shift();
+        const elementRemove = document.getElementById("elements");
+        elementRemove.parentNode.removeChild(elementRemove);
+        updateDequeue();
+    }
+};
+
+const updateEnqueue = () => {
+    aux = 0;
+
+    queue.forEach((element) => {   
+        aux++;
+        if (aux < queue.length) {
+            return;
+        }
+           
+        printQueue.innerHTML += `<p class="mirror-filter queue-screen_elements" id="elements"> ${aux}. ${element}</p>`;
+    });
+};
+
+const updateDequeue = () => {
+    aux = 1;
+    printQueue.innerHTML = ``;
+    queue.forEach((element) => {   
+        printQueue.innerHTML += `<p class="mirror-filter queue-screen_elements" id="elements"> ${aux}. ${element}</p>`;
+        aux++;
+    });
+};
 
 enqueueButton.addEventListener("click", () => {
+    if (!isNaN(user.value)) {
+        return;
+    }
+
+    if (queue.includes(user.value)) {
+        return;
+    }
+
     enqueue(user.value);
+    user.value = "";
+    updateEnqueue();
 });
 
-
-
+dequeueButton.addEventListener("click", () => {
+    dequeue();
+    updateDequeue();
+});
