637f635a6c6b3f10c5541bc33270f4392296d99b
Rian Delou <riandelou63@hotmail.com>
2024-04-05 22:33:33 -0300

    new update

--- script.js
+++ script.js
@@ -24,20 +24,32 @@ const dequeue = () => {
 const updateEnqueue = () => {
     aux = 0;
 
-    queue.forEach((element) => {   
+    queue.forEach((element) => {
         aux++;
+
+        if (queue.length == 1) { // base case to update
+            printQueue.innerHTML = `<p class="mirror-filter queue-screen_elements" id="elements"> ${aux}. ${element}</p>`;
+            return;
+        }
+
         if (aux < queue.length) {
             return;
         }
-           
+
         printQueue.innerHTML += `<p class="mirror-filter queue-screen_elements" id="elements"> ${aux}. ${element}</p>`;
     });
 };
 
 const updateDequeue = () => {
+
+    if (queue.length == 0) { // base case to update
+        printQueue.innerHTML =  `<p class="mirror-filter queue-screen_elements" id="elements">Waiting a name...</p>`;
+        return;
+    }
+
     aux = 1;
     printQueue.innerHTML = ``;
-    queue.forEach((element) => {   
+    queue.forEach((element) => {
         printQueue.innerHTML += `<p class="mirror-filter queue-screen_elements" id="elements"> ${aux}. ${element}</p>`;
         aux++;
     });
